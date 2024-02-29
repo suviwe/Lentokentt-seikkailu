dfa7ff7402e21a11a298db13df249b69eee78856
suviwe <suvi.westerberg@metropolia.fi>
2024-02-29 13:20:29 +0200

    lentopelin story

---
+++ story.py
@@ -0,0 +1,9 @@
+screen_name = input("Hi there, what is your name?")
+airplane_name = input("Hi " + screen_name + ", now you can choose name for your airplane, what would you like to call it?")
+
+story = ("Welcome " + screen_name + ", to the flight adventure!"
+         "In this game, you start your journey with your plane " + airplane_name + " from a randomly selected European airport and choose your next destination from three nearby airports."
+         "As you travel, you collect points, but remember that your battery consumes energy. "
+         "The game ends when the battery is empty. Good luck on your journey and collect as many points as possible!")
+
+print(story)
