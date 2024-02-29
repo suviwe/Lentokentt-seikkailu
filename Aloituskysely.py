da13692b4bd19e19d70f53d7aa4dafe56d6e2ce4
TaruNipuli <taru.nipuli@metropolia.fi>
2024-02-29 11:04:52 +0200

    Toimivuutta paranneltu sekä lisätty alkuun selite koodin toiminnasta

--- Aloituskysely.py
+++ Aloituskysely.py
@@ -1,3 +1,9 @@
+# Pelin aloitus, jossa pelaajalta kysytään haluaako hän aloittaa uuden pelin
+# vai katsoa edellisten pelien tuloksia.
+# Pelin voi aloittaa tulosten katsomisen jälkeen painamalla 1 tai lopettaa ohjelman painamalla enteriä.
+
+
+
 import mysql.connector
 
 yhteys = mysql.connector.connect(
@@ -9,7 +15,7 @@ yhteys = mysql.connector.connect(
     autocommit=True
 )
 
-# Funktio, joka kysyy pelaajalta, haluaako hän aloittaa uuden pelin vai katsoa pisteet
+# Funktio, joka kysyy pelaajalta, haluaako hän aloittaa uuden pelin vai katsoa pisteet.
 def player_choice():
     while True:
         choice = input("Do you want to start a new game (1) or see player scores (2)? ")
@@ -20,7 +26,7 @@ def player_choice():
 
 # Funktio hakee pistetiedot tietokannasta
 def get_scores():
-    sql = "SELECT * FROM player_scores"
+    sql = ("SELECT * FROM player_scores order by score desc limit 3")
     cursor = yhteys.cursor()
     cursor.execute(sql)
     results = cursor.fetchall()
@@ -28,17 +34,27 @@ def get_scores():
 
 # Funktio, joka tulostaa pisteet ja kysyy pelaajalta, haluaako hän aloittaa uuden pelin tai lopettaa ohjelman
 def print_scores_and_ask_for_new_game(results):
-    if results:
-        print("Player scores: ")
-        for result in results:
-            print(f"Username: {result[1]}")
-            print(f"Aircraft: {result[2]}")
-            print(f"Score: {result[3]}")
-            print()  # Tyhjä rivi erottamaan eri pelaajien tulokset toisistaan
-        if input("Press (1) to start a new game or press enter to quit ") == "1":
-            print("New adventure begins!")  # pelin aloitus
-    else:
-        print("No scores saved.")
+    while True:
+        if results:
+            print("Player scores: ")
+            for result in results:
+                print(f"Username: {result[1]}")
+                print(f"Aircraft: {result[2]}")
+                print(f"Score: {result[3]}")
+                print()  # Tyhjä rivi erottamaan eri pelaajien tulokset toisistaan
+            choice = input("Press (1) to start a new game or press enter to quit ")
+            if choice == "":
+                print("You closed the program.")
+                return
+            elif choice == "1":
+                print("New adventure begins!")
+                return
+            else:
+                print("Invalid choice. Press 1 or enter.")
+        else:
+            print("No scores saved.")
+            return
+
 
 choice = player_choice() # Kutsutaan player_choice funktiota pelaajan valinnan mukaan
 
